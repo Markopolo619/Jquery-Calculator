279ad52bca9ebe83343f8fbfd82f3dc03b8c0af1
Markopolo619 <markmadi123@protonmail.com>
2021-09-24 11:50:35 +0200

    Created variables ofr the symbols

---
+++ src/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    const numberButtons = $('[data-number]')
+    const operationButtons = $('[data-operation]')
+    const equalsButton = $('[data-equals]')
+    const deleteButton = $('[data-delete]')
+    const allClearButton = $('[data-all-clear]')
+    const previousOperandTextElement = $('[data-previous-operand]')
+    const currentOperandTextElement = $('[data-current-operand]')
+    
+  });
